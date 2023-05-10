4b646da63b9731c36970ce4368a35bf770cc1cc1
Mellamoberto <app-del-96@hotmail.com>
2023-05-10 09:16:28 +0200

    Changes

--- Proyecto/informacion.js
+++ Proyecto/informacion.js
@@ -1,6 +1,5 @@
 
 function informacion() {
-    document.getElementById("mensaje").innerHTML = "Más información";
     alert("Nombre: Alberto Palomo Pino \nCurso: 1º DAM \nTema: Diario de Videojuegos")
 }
 
