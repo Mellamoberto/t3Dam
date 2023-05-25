cfaa5d02113a298453d9ab7e285fcd7ac3f3e480
Mellamoberto <app-del-96@hotmail.com>
2023-05-25 13:29:19 +0200

    Trabajo LMarcas

--- Proyecto/informacion.js
+++ Proyecto/informacion.js
@@ -1,4 +1,17 @@
 
-function informacion() {
-    alert("Nombre: Alberto Palomo Pino \nCurso: 1º DAM \nTema: Diario de Videojuegos");
-}
+document.addEventListener("DOMContentLoaded", function() {
+    const openModalBtn = document.querySelector(".modal-link");
+    const modal = document.getElementById("modal");
+    const closeBtn = document.querySelector(".close");
+
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    openModalBtn.addEventListener("click", openModal);
+    closeBtn.addEventListener("click", closeModal);
+});
---
+++ Proyecto/menu.js
@@ -0,0 +1,46 @@
+const menuContainer = document.getElementById('menu-container');
+
+menuContainer.innerHTML = `
+<body>
+<nav class="menu">
+    <button class="menu">
+        <a class="menu" href="index.html">Daily-Plays</a>
+    </button>
+
+    <button class="menu">
+        <a class="menu" href="./juego.html" target="_blank">Página para ver un juego</a>
+    </button>
+
+    <button class="menu">
+    <a class="modal-link" href="#">Información Web</a>
+    </button>
+
+    <button class="menu">
+        <a class="menu" id="mensaje" href="#">XML</a>
+    </button>
+
+    <button class="menu">
+        <a class="menu" href="./ajustes.html">Ajustes</a>
+    </button>
+</nav>
+
+<script src="modal.js"></script>
+</body>
+`;
+
+document.addEventListener("DOMContentLoaded", function() {
+    const openModalBtn = document.querySelector(".modal-link");
+    const modal = document.getElementById("modal");
+    const closeBtn = document.querySelector(".close");
+
+    function openModal() {
+        modal.style.display = "block";
+    }
+
+    function closeModal() {
+        modal.style.display = "none";
+    }
+
+    openModalBtn.addEventListener("click", openModal);
+    closeBtn.addEventListener("click", closeModal);
+});
